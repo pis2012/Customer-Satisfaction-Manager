7ab6153acadcf3f178156390b7908bc1bb607fb5
fedmuj <federicomujica1@gmail.com>
2012-09-24 16:02:12 -0300

    Css al index de feebacks

--- test/unit/feedback_test.rb
+++ test/unit/feedback_test.rb
@@ -6,7 +6,10 @@ class FeedbackTest < ActiveSupport::TestCase
   # end
 
 
-
+  test "no debe guardar el feedback" do
+    feed = Feedback.new
+    assert !Feedback.save, "Saved the post without a title"
+  end
 
 
 end
