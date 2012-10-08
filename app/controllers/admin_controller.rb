04d2158444492f5d812a6ddfa8d842707c30e18b
gruiz90 <germanruizravi@gmail.com>
2012-10-08 15:03:44 -0200

    Arreglos varios en vista de proyecto y controlador de admins.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,21 +8,21 @@ class AdminController < ApplicationController
     data.map! {|d| d = 0}
     axis = Array.new(12)
 
-    fechaInicial = Time.now - 1.year + 1.month
-    fecha = fechaInicial
+    initial_date = Time.now - 1.year + 1.month
+    date = initial_date
     for i in 0..11
-      axis[i] = fecha.strftime("%b")
-      fecha = fecha + 1.month
+      axis[i] = date.strftime("%b")
+      date = date + 1.month
     end
 
     cont = Array.new(12)
     cont.map! {|c| c = 0}
-    moods = Mood.all.select { |mood| mood.created_at <= fecha && mood.created_at >= fechaInicial }
+    moods = Mood.all.select { |mood| mood.created_at <= date && mood.created_at >= initial_date }
 
     moods.each do |mood|
-      mes = mood.created_at.month - 1
-      data[mes] += mood.status
-      cont[mes] += 1
+      month = mood.created_at.month - 1
+      data[month] += mood.status
+      cont[month] += 1
     end
 
     cont.map! do |c|
@@ -48,8 +48,8 @@ class AdminController < ApplicationController
     end
 
     total = Project.count
-    labels = data.map do |numero|
-      "(#{total == 0 ? 0 : (100.0 * numero / total).round}%)"
+    labels = data.map do |number|
+      "(#{total == 0 ? 0 : (100.0 * number / total).round}%)"
     end
     @graph[:pie] = Gchart.pie_3d(:labels => labels, :data => data, :size => '550x250',
                                    :bar_colors => ['FF0000','FFA000','FFFF00','00FFA0','00FF00'],
