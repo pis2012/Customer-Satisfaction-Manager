0cee6aebe4545dc39773abf7284d6a453e801632
santiago <santiagobianchigallo@hotmail.com>
2012-10-08 20:34:41 -0200

    unit test

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "ensure_not_referenced_by_any_project" do
+     client = Client.new
+     assert client.ensure_not_referenced_by_any_project
+   end
+
+   test "ensure_not_referenced_by_any_user" do
+     client = Client.new
+     assert client.ensure_not_referenced_by_any_user
+   end
 end
--- test/unit/mood_test.rb
+++ test/unit/mood_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class MoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get_mood_img" do
+    mood = Mood.new
+    mood.status = 1
+    assert mood.get_mood_img == "1.png"
+  end
 end
