76bdde9552e3098bf0768e0818ec523f8c773180
matiasperes <matiasperes23@gmail.com>
2012-10-29 10:22:27 -0200

    Summary sin estilo.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -35,12 +35,12 @@ class Activity
       # feedbacks are more relevant than moods
       if feedbacks.length > 0 && activities_limit > 0
         feedback = feedbacks.first
-        new_activity = Activity.new(action: "New Feedback: " + feedback.subject,
-                                     author: feedback.user.full_name,
-                                     date_time: feedback.created_at,
-                                     image_path: feedback.feedback_type.image_url,
-                                     link: '/projects/change_profile_project_path/' + feedback.project.id.to_s,
-                                     content: feedback.content )
+        new_activity = Activity.new("New Feedback: " + feedback.subject,
+                                     feedback.user.full_name,
+                                     feedback.created_at.to_date,
+                                     feedback.feedback_type.image_url,
+                                     '/projects/change_profile_project/' + feedback.project.id.to_s,
+                                     feedback.content )
         activities.push new_activity
         feedbacks.pop
         activities_limit = activities_limit - 1
@@ -49,22 +49,28 @@ class Activity
       # moods are more relevant than comments
       if moods.length > 0 && activities_limit > 0
         mood = moods.first
-        activities.push Activity.new(action: "Mood Update!",
-                                     author: mood.project.client.name,
-                                     date_time: mood.created_at,
-                                     image_path: 'moods/normal/' + mood.get_mood_img,
-                                     link: '/projects/change_profile_project_path/' + mood.project.id.to_s )
+        activities.push Activity.new("Mood Update!",
+                                     mood.project.client.name,
+                                     mood.created_at,
+                                     'moods/normal/' + mood.get_mood_img,
+                                     '/projects/change_profile_project/' + mood.project.id.to_s )
         moods.pop
         activities_limit = activities_limit - 1
       end
 
       if comments.length > 0 && activities_limit > 0
         comment = comments.first
-        activities.push Activity.new(action: "New Comment",
-                                     author: comment.user.full_name,
-                                     date_time: comment.created_at,
-                                     image_path: 'moods/normal/' + comment.get_mood_img,
-                                     link: '/projects/change_profile_project_path/' + comment.feedback.project.id.to_s )
+        if comment.user.profile.avatar.file? && !comment.user.profile.show_gravatar
+             picture = comment.user.profile.avatar.url(:medium)
+        else
+             picture = comment.user.profile.user.email.gsub('spam', 'mdeering')
+        end
+
+        activities.push Activity.new("New comment on feedback " + comment.feedback.subject,
+                                     comment.user.full_name,
+                                     comment.created_at,
+                                     picture,
+                                     '/projects/change_profile_project/' + comment.feedback.project.id.to_s )
         comments.pop
         activities_limit = activities_limit - 1
       end
