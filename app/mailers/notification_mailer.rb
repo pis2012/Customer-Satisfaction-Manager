37555be2d3eb57806e500eacc0d0a69e48220043
germanmarcone <hellbentcaretaker@gmail.com>
2012-11-01 16:10:10 -0200

    Email de peticion de estado

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,22 +2,22 @@ class NotificationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
 
+  def addInlineImage(name,location)
+    attachments.inline[name] = {
+        :data => File.read("#{Rails.root.to_s + location}"),
+        :mime_type => "image/png",
+        :encoding => "base64"
+    }
+  end
+
   def feedback_notification_email(feedback,user)
     @feedback = feedback
     @project = Project.find(feedback.project_id)
     @user = user
     @profile = user.profile
-    attachments.inline['logo'] = {
-        :data => File.read("#{Rails.root.to_s + '/app/assets/images/moove-it.png'}"),
-        :mime_type => "image/png",
-        :encoding => "base64"
-    }
-    attachments.inline['header_bg'] = {
-        :data => File.read("#{Rails.root.to_s + '/app/assets/images/header_bg.png'}"),
-        :mime_type => "image/png",
-        :encoding => "base64"
-    }
-    mail(:to => user.email, :subject => "New Feedback for your project in Moove-it")
+    addInlineImage('logo','/app/assets/images/moove-it.png')
+
+    mail(:to => user.email, :subject => "New Feedback for your project in Moove-iT")
   end
 
   def comment_notification_email(comment,user)
@@ -25,12 +25,23 @@ class NotificationMailer < ActionMailer::Base
     @project = Project.find(comment.feedback.project_id)
     @user = user
     @profile = user.profile
-    attachments.inline['logo'] = {
-        :data => File.read("#{Rails.root.to_s + '/app/assets/images/moove-it.png'}"),
-        :mime_type => "image/png",
-        :encoding => "base64"
-    }
-    mail(:to => user.email, :subject => "New Comment for your project in Moove-it")
+    addInlineImage('logo','/app/assets/images/moove-it.png')
+    mail(:to => user.email, :subject => "New Comment for your project in Moove-iT")
+  end
+
+  def reminder_email(project,user)
+    @project = project
+    @user = user
+    @profile = user.profile
+    addInlineImage('logo','/app/assets/images/moove-it.png')
+
+    addInlineImage('Angry','/app/assets/images/moods/small/1.png')
+    addInlineImage('Disappointed','/app/assets/images/moods/small/2.png')
+    addInlineImage('Indifferent','/app/assets/images/moods/small/3.png')
+    addInlineImage('Pleased','/app/assets/images/moods/small/4.png')
+    addInlineImage('Happy','/app/assets/images/moods/small/5.png')
+
+    mail(:to => user.email, :subject => "Friendly reminder from Moove-iT")
   end
 
 end
